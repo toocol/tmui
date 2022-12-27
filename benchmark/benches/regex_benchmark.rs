5c64c74a04cc1ee22d6d2be3e092184f0d3ca801
JoeZane <joezane.cn@gmail.com>
2022-12-28 01:16:30 +0800

    1. update regex_benchmark.rs

--- benchmark/benches/regex_benchmark.rs
+++ benchmark/benches/regex_benchmark.rs
@@ -5,21 +5,26 @@ const MATCH_STR: &'static str =
     "wwww.google.com fsdjflkadjf.com https://www.google.com http://google.com fjdsklfjasljflsajflsajf fjsdalkfjsldjf https://meta.com fdsjfklsdjfaljlj https://3213@.com fjsdaklfjsladjflsjadflkjsdafjslajflasjflaksjflkasjflksajf fdsjfklsdjfaljlj https://3213@.com fjsdaklfjsladjflsjadflkjsdafjslajflasjflaksjflkasjflksajf xxx.xx@gmail.com @^#,@gmailc.om fdsfdsf.com fsdf123@23.com fdsjfklsdjfaljlj https://3213@.com fjsdaklfjsladjflsjadflkjsdafjslajflasjflaksjflkasjflksajf fdfsf@gds.com";
 
 fn test_regex(regex: &Regex) {
-    let caps = regex.captures(MATCH_STR);
-    assert!(caps.unwrap().len() > 0);
+    let mut captured_text = vec![];
+    for cap in regex.captures_iter(MATCH_STR) {
+        for matched in cap.iter() {
+            if let Some(m) = matched {
+                captured_text.push(m.as_str().to_string());
+            }
+        }
+    }
+    assert!(captured_text.len() > 0)
 }
 
-fn test_pcre2(regex: &pcre2::bytes::Regex) {
-    let caps = regex.captures(MATCH_STR.as_bytes()).unwrap();
-    assert!(caps.unwrap().len() > 0);
+fn test_regex_clone(regex: &Regex) {
+    let _ = regex.clone();
 }
 
 pub fn criterion_values(c: &mut Criterion) {
-    let regex = Regex::new(r"([a-zA-z]+://[^\s]*)|(^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$)").unwrap();
-    let pcre2 = pcre2::bytes::Regex::new(r"([a-zA-z]+://[^\s]*)|(^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$)").unwrap();
+    let regex = Regex::new(r"([a-zA-z]+://[^\s]*)|([\w!#$%&'*+/=?^_`{|}~-]+(?:\.[\w!#$%&'*+/=?^_`{|}~-]+)*@(?:[\w](?:[\w-]*[\w])?\.)+[\w](?:[\w-]*[\w])?)").unwrap();
 
     c.bench_function("regex-regex-test", |b| b.iter(|| test_regex(&regex)));
-    c.bench_function("regex-pcre2-test", |b| b.iter(|| test_pcre2(&pcre2)));
+    c.bench_function("regex-clone-test", |b| b.iter(|| test_regex_clone(&regex)));
 }
 
 criterion_group!(benches, criterion_values);
