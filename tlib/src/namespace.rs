df8c04747423d4604d7f7f98a227be9256e1a538
Joe Zane <joezane.cn@gmail.com>
2022-12-28 20:39:39 +0800

    1. Update KeyboardModifier

--- tlib/src/namespace.rs
+++ tlib/src/namespace.rs
@@ -1,4 +1,5 @@
 #[repr(u32)]
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
 pub enum KeyboardModifier {
     NoModifier = 0x00000000,
     ShiftModifier = 0x02000000,
@@ -9,18 +10,37 @@ pub enum KeyboardModifier {
     GroupSwitchModifier = 0x40000000,
 
     KeyboardModifierMask = 0xfe000000,
+    Combination(u32),
+}
+impl KeyboardModifier {
+    pub fn or(self, other: KeyboardModifier) -> KeyboardModifier {
+        let one: u32 = self.into();
+        let other: u32 = other.into();
+        Self::Combination(one | other)
+    }
+
+    pub fn has(&self, has: KeyboardModifier) -> bool {
+        match self {
+            Self::Combination(mask) => {
+                let has: u32 = has.into();
+                mask & has > 0
+            },
+            _ => *self == has,
+        }
+    }
 }
 impl Into<u32> for KeyboardModifier {
     fn into(self) -> u32 {
         match self {
-            Self::NoModifier => Self::NoModifier as u32,
-            Self::ShiftModifier => Self::ShiftModifier as u32,
-            Self::ControlModifier => Self::ControlModifier as u32,
-            Self::AltModifier => Self::AltModifier as u32,
-            Self::MetaModifier => Self::MetaModifier as u32,
-            Self::KeypadModifier => Self::KeypadModifier as u32,
-            Self::GroupSwitchModifier => Self::GroupSwitchModifier as u32,
-            Self::KeyboardModifierMask => Self::KeyboardModifierMask as u32,
+            Self::NoModifier => 0x00000000,
+            Self::ShiftModifier => 0x02000000,
+            Self::ControlModifier => 0x04000000,
+            Self::AltModifier => 0x08000000,
+            Self::MetaModifier => 0x10000000,
+            Self::KeypadModifier => 0x20000000,
+            Self::GroupSwitchModifier => 0x40000000,
+            Self::KeyboardModifierMask => 0xfe000000,
+            Self::Combination(mask) => mask,
         }
     }
 }
