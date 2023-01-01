aea68d7b293a31c24385a3b48ccf570ad11f3cf0
JoeZane <joezane.cn@gmail.com>
2023-01-01 20:11:26 +0800

    1. Add new struct Size in figure.rs

--- tmui/src/graphics/figure.rs
+++ tmui/src/graphics/figure.rs
@@ -8,7 +8,9 @@ use tlib::{
     Value,
 };
 
-/////////////// Point
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+/// Point
+//////////////////////////////////////////////////////////////////////////////////////////////////////
 #[derive(Debug, Default, PartialEq, Eq, Clone, Copy)]
 pub struct Point {
     x: i32,
@@ -122,7 +124,100 @@ impl FromValue for Point {
     }
 }
 
-/////////////// Rectangle
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+/// Size
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+#[derive(Debug, Default, PartialEq, Eq, Clone, Copy)]
+pub struct Size {
+    width: i32,
+    height: i32,
+}
+impl Size {
+    pub fn new(width: i32, height: i32) -> Self {
+        Self { width, height }
+    }
+
+    pub fn width(&self) -> i32 {
+        self.width
+    }
+
+    pub fn height(&self) -> i32 {
+        self.height
+    }
+}
+impl From<(i32, i32)> for Size {
+    fn from((width, height): (i32, i32)) -> Self {
+        Self { width, height }
+    }
+}
+impl Into<(i32, i32)> for Size {
+    fn into(self) -> (i32, i32) {
+        (self.width, self.height)
+    }
+}
+impl Add for Size {
+    type Output = Size;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Self {
+            width: self.width + rhs.width,
+            height: self.height + rhs.height,
+        }
+    }
+}
+impl Sub for Size {
+    type Output = Size;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        Self {
+            width: self.width - rhs.width,
+            height: self.height - rhs.height,
+        }
+    }
+}
+
+impl StaticType for Size {
+    fn static_type() -> tlib::Type {
+        tlib::Type::from_name("Point")
+    }
+
+    fn bytes_len() -> usize {
+        i32::bytes_len() + i32::bytes_len()
+    }
+}
+impl ToBytes for Size {
+    fn to_bytes(&self) -> Vec<u8> {
+        let mut bytes = vec![];
+        bytes.append(&mut self.width.to_bytes());
+        bytes.append(&mut self.height.to_bytes());
+        bytes
+    }
+}
+impl FromBytes for Size {
+    fn from_bytes(data: &[u8], _len: usize) -> Self {
+        let width = i32::from_bytes(&data[0..4], 4);
+        let height = i32::from_bytes(&data[4..8], 4);
+        Self { width, height }
+    }
+}
+impl ToValue for Size {
+    fn to_value(&self) -> tlib::Value {
+        tlib::Value::new(self)
+    }
+
+    fn value_type(&self) -> tlib::Type {
+        Self::static_type()
+    }
+}
+impl FromValue for Size {
+    fn from_value(value: &Value) -> Self {
+        Self::from_bytes(value.data(), Self::bytes_len())
+    }
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+/// Rectangle
+//////////////////////////////////////////////////////////////////////////////////////////////////////
 #[derive(Debug, Default, PartialEq, Eq, Clone, Copy)]
 pub struct Rect {
     x: i32,
@@ -260,7 +355,9 @@ impl FromValue for Rect {
     }
 }
 
-/////////////// Color
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+/// Color
+//////////////////////////////////////////////////////////////////////////////////////////////////////
 #[derive(Debug, Default, PartialEq, Eq, Clone, Copy)]
 pub struct Color {
     pub r: u8,
@@ -430,6 +527,14 @@ mod tests {
         let get = val.get::<Point>();
         assert_eq!(20, get.x);
         assert_eq!(20, get.y);
+    }
+
+    #[test]
+    fn test_size() {
+        let size = Size::new(100, 100);
+        let val = size.to_value();
+        let get = val.get::<Size>();
+        assert_eq!(size, get);
     }
 
     #[test]
