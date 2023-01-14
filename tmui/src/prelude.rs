d6142bcf505aaecea81a6dc69fb5d26b8b97cd85
Joe Zane <joezane.cn@gmail.com>
2023-01-14 15:39:21 +0800

    1.  pub use skia_safe font moudle

--- tmui/src/prelude.rs
+++ tmui/src/prelude.rs
@@ -1,7 +1,8 @@
 pub use tlib::prelude::*;
 pub use tlib::signal;
+pub use std::cell::Ref;
+pub use skia_safe::font::*;
 
-pub use std::{any::Any, cell::Ref};
 pub use crate::graphics::drawing_context::DrawingContext;
 pub use crate::graphics::element::{Element, ElementExt, ElementAcquire, ElementImpl};
 pub use crate::graphics::figure::{Point, Rect};
