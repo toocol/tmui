f3e3de467b97e2ea85310d7cde096b3622f12238
JoeZane <joezane.cn@gmail.com>
2023-05-08 00:51:47 +0800

    1. add boost lib dir to rustc link search path

--- tipc/build.rs
+++ tipc/build.rs
@@ -3,9 +3,14 @@ use std::{path::PathBuf, env};
 fn main() {
     let library_adapter = "ipc-native";
     let library_dir = PathBuf::from(env::var_os("CARGO_MANIFEST_DIR").unwrap());
+    let boost_dir = PathBuf::from(env::var_os("BOOST_LIB_DIR").unwrap());
     println!("cargo:rustc-link-lib=static={}", library_adapter);
     println!(
         "cargo:rustc-link-search=native={}",
         env::join_paths(&[library_dir]).unwrap().to_str().unwrap()
+    );
+    println!(
+        "cargo:rustc-link-search=native={}",
+        env::join_paths(&[boost_dir]).unwrap().to_str().unwrap()
     );
 }
