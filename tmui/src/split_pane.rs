50c3bab6aebdb8eb587b0f805d12dd5c6bbc23a6
Joe Zane <joezane.cn@gmail.com>
2023-06-03 18:44:01 +0800

    Update the codes of SplitPane

--- tmui/src/split_pane.rs
+++ tmui/src/split_pane.rs
@@ -6,14 +6,14 @@ use crate::{
     },
     widget::WidgetImpl,
 };
-use std::{mem::size_of, collections::HashMap};
+use std::{collections::HashMap, mem::size_of};
 use tlib::{implements_enum_value, namespace::AsNumeric};
 
 #[extends(Container)]
 #[derive(Default)]
 pub struct SplitPane {
     split_state: SplitState,
-    locations: HashMap<ViewLocation, u16>,
+    locations: HashMap<u16, ViewLocation>,
 }
 
 impl ObjectSubclass for SplitPane {
@@ -74,7 +74,66 @@ impl ContainerLayout for SplitPane {
 }
 
 pub trait SplitPaneExt {
+    /// Change the split state of SplitPane.
     fn change_state(&mut self, state: SplitState);
+
+    /// Split new widget left.
+    ///
+    /// @param: id the id of target widget where user click on. <br>
+    /// @param widget the new widget that split off. <br>
+    ///
+    /// @reutrn success or not.
+    fn split_left<T: WidgetImpl>(&mut self, id: u16, widget: T) -> bool;
+
+    /// Split new widget up.
+    ///
+    /// @param: id the id of target widget where user click on. <br>
+    /// @param widget the new widget that split off. <br>
+    ///
+    /// @reutrn success or not.
+    fn split_up<T: WidgetImpl>(&mut self, id: u16, widget: T);
+
+    /// Split new widget right.
+    ///
+    /// @param: id the id of target widget where user click on. <br>
+    /// @param widget the new widget that split off. <br>
+    ///
+    /// @reutrn success or not.
+    fn split_right<T: WidgetImpl>(&mut self, id: u16, widget: T);
+
+    /// Split new widget down.
+    ///
+    /// @param: id the id of target widget where user click on. <br>
+    /// @param widget the new widget that split off. <br>
+    ///
+    /// @reutrn success or not.
+    fn split_down<T: WidgetImpl>(&mut self, id: u16, widget: T);
+}
+
+impl SplitPaneExt for SplitPane {
+    fn change_state(&mut self, state: SplitState) {
+        todo!()
+    }
+
+    fn split_left<T: WidgetImpl>(&mut self, id: u16, widget: T) -> bool {
+        let view_location = *self.locations.get(&id).unwrap();
+        if !view_location.check_split_left(&mut self.locations) {
+            return false;
+        }
+        true
+    }
+
+    fn split_up<T: WidgetImpl>(&mut self, id: u16, widget: T) {
+        todo!()
+    }
+
+    fn split_right<T: WidgetImpl>(&mut self, id: u16, widget: T) {
+        todo!()
+    }
+
+    fn split_down<T: WidgetImpl>(&mut self, id: u16, widget: T) {
+        todo!()
+    }
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////
@@ -130,6 +189,10 @@ pub enum ViewLocation {
     ThreeRightTop,
     ThreeRightBottom,
 
+    ThreeRight,
+    ThreeLeftTop,
+    ThreeLeftBottom,
+
     // Split screen for four
     FourLeftTop,
     FourLeftBottom,
@@ -145,10 +208,13 @@ impl AsNumeric<u8> for ViewLocation {
             Self::ThreeLeft => 3,
             Self::ThreeRightTop => 4,
             Self::ThreeRightBottom => 5,
-            Self::FourLeftTop => 6,
-            Self::FourLeftBottom => 7,
-            Self::FourRightTop => 8,
-            Self::FourRightBottom => 9,
+            Self::ThreeRight => 6,
+            Self::ThreeLeftTop => 7,
+            Self::ThreeLeftBottom => 8,
+            Self::FourLeftTop => 9,
+            Self::FourLeftBottom => 10,
+            Self::FourRightTop => 11,
+            Self::FourRightBottom => 12,
         }
     }
 }
@@ -161,12 +227,66 @@ impl From<u8> for ViewLocation {
             3 => Self::ThreeLeft,
             4 => Self::ThreeRightTop,
             5 => Self::ThreeRightBottom,
-            6 => Self::FourLeftTop,
-            7 => Self::FourLeftBottom,
-            8 => Self::FourRightTop,
-            9 => Self::FourRightBottom,
+            6 => Self::ThreeRight,
+            7 => Self::ThreeLeftTop,
+            8 => Self::ThreeLeftBottom,
+            9 => Self::FourLeftTop,
+            10 => Self::FourLeftBottom,
+            11 => Self::FourRightTop,
+            12 => Self::FourRightBottom,
             _ => unimplemented!(),
         }
     }
 }
 implements_enum_value!(ViewLocation, u8);
+impl ViewLocation {
+    /// Check the widget can split left or not.
+    #[inline]
+    pub fn check_split_left(self, view_locations: &mut HashMap<u16, ViewLocation>) -> bool {
+        match self {
+            Self::OneCenter => true,
+            Self::TwoRight => true,
+            Self::ThreeRightTop => true,
+            Self::ThreeRightBottom => true,
+            _ => false,
+        }
+    }
+
+    /// Check the widget can split up or not.
+    #[inline]
+    pub fn check_split_up(self) -> bool {
+        match self {
+            Self::OneCenter => true,
+            Self::TwoLeft => true,
+            Self::TwoRight => true,
+            Self::ThreeLeft => true,
+            Self::ThreeRight => true,
+            _ => false,
+        }
+    }
+
+    /// Check the widget can split right or not.
+    #[inline]
+    pub fn check_split_right(self) -> bool {
+        match self {
+            Self::OneCenter => true,
+            Self::TwoLeft => true,
+            Self::ThreeLeftTop => true,
+            Self::ThreeLeftBottom => true,
+            _ => false,
+        }
+    }
+
+    /// Check the widget can split down or not.
+    #[inline]
+    pub fn check_split_down(self) -> bool {
+        match self {
+            Self::OneCenter => true,
+            Self::TwoLeft => true,
+            Self::TwoRight => true,
+            Self::ThreeLeft => true,
+            Self::ThreeRight => true,
+            _ => false,
+        }
+    }
+}
