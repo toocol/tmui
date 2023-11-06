86189bb8f15bacafe1976405558dc1609fc57014
Joe Zane <joezane.cn@gmail.com>
2023-11-06 19:53:21 +0800

    `render_difference.rs` add codes to handle child with paddings.

--- tmui/src/graphics/render_difference.rs
+++ tmui/src/graphics/render_difference.rs
@@ -34,27 +34,41 @@ impl<T: WidgetImpl> RenderDiffence for T {}
 pub trait ChildWidgetDiffRender: WidgetImpl {
     fn widget_diff_render(&mut self, painter: &mut Painter) {
         if let Some(child) = self.get_child_ref() {
+            // Handle child with margins:
             let rec_rect = child.image_rect_record();
             let cur_rect = child.rect();
 
-            if rec_rect == cur_rect {
-                return;
+            if rec_rect != cur_rect {
+                let intersects: skia_safe::IRect = rec_rect
+                    .intersects(&cur_rect)
+                    .unwrap_or((0, 0, 0, 0).into())
+                    .into();
+                let rec_rect: skia_safe::IRect = rec_rect.into();
+
+                let mut clear_region = skia_safe::Region::new();
+                clear_region.op_rect(rec_rect, RegionOp::Union);
+                clear_region.op_rect(intersects, RegionOp::Difference);
+
+                painter.save();
+                painter.clip_region(clear_region, ClipOp::Intersect);
+                painter.fill_rect(rec_rect, self.background());
+                painter.restore();
             }
 
-            let intersects: skia_safe::IRect = rec_rect
-                .intersects(&cur_rect)
-                .unwrap_or((0, 0, 0, 0).into())
-                .into();
-            let rec_rect: skia_safe::IRect = rec_rect.into();
+            // Handle child with paddings:
+            let rec_rect: skia_safe::IRect = child.rect().into();
+            let child_content_rect: skia_safe::IRect = child.contents_rect(None).into();
 
-            let mut clear_region = skia_safe::Region::new();
-            clear_region.op_rect(rec_rect, RegionOp::Union);
-            clear_region.op_rect(intersects, RegionOp::Difference);
+            if rec_rect != child_content_rect {
+                let mut clear_region = skia_safe::Region::new();
+                clear_region.op_rect(rec_rect, RegionOp::Union);
+                clear_region.op_rect(child_content_rect, RegionOp::Difference);
 
-            painter.save();
-            painter.clip_region(clear_region, ClipOp::Intersect);
-            painter.fill_rect(rec_rect, self.background());
-            painter.restore();
+                painter.save();
+                painter.clip_region(clear_region, ClipOp::Intersect);
+                painter.fill_rect(rec_rect, self.background());
+                painter.restore();
+            }
         }
     }
 }
