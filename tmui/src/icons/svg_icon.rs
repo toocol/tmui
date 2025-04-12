ba22ced1d8e04330f40a70cf9f1fb84c69c54715
JoeZane <joezane.cn@gmail.com>
2025-04-13 04:18:57 +0800

    upd: add `clear` to MemQueue, update SvgIcon

--- tmui/src/icons/svg_icon.rs
+++ tmui/src/icons/svg_icon.rs
@@ -4,11 +4,7 @@ use crate::{
     widget::{widget_inner::WidgetInnerExt, WidgetImpl},
 };
 use std::io::Read;
-use tlib::{
-    connect,
-    skia_safe::FontMgr,
-    typedef::SkiaSvgDom,
-};
+use tlib::{skia_safe::FontMgr, typedef::SkiaSvgDom};
 use usvg::{fontdb::Database, Options, Tree};
 
 /// TODO: Wait to improve, see https://github.com/rust-skia/rust-skia/discussions/928
@@ -16,29 +12,27 @@ use usvg::{fontdb::Database, Options, Tree};
 pub struct SvgIcon {
     dom: Option<SkiaSvgDom>,
     view_size: Size,
-    origin: FPoint,
 }
 
 impl ObjectSubclass for SvgIcon {
     const NAME: &'static str = "SvgIcon";
 }
 
-impl ObjectImpl for SvgIcon {
-    fn initialize(&mut self) {
-        connect!(
-            self,
-            geometry_changed(),
-            self,
-            handle_geometry_changed(FRect)
-        );
-    }
-}
+impl ObjectImpl for SvgIcon {}
 
 impl WidgetImpl for SvgIcon {
     fn paint(&mut self, painter: &mut Painter) {
+        let rect = self.rect_f();
+        let (x1, y1, w1, h1) = (rect.x(), rect.y(), rect.width(), rect.height());
+        let (w2, h2) = (
+            self.view_size.width() as f32,
+            self.view_size.height() as f32,
+        );
+        let origin = FPoint::new(x1 + (w1 - w2) / 2., y1 + (h1 - h2) / 2.);
+
         if let Some(ref dom) = self.dom {
             painter.save();
-            painter.translate(self.origin.x(), self.origin.y());
+            painter.translate(origin.x(), origin.y());
             painter.draw_dom(dom);
             painter.restore();
         }
@@ -87,12 +81,5 @@ impl SvgIcon {
         let dom = SkiaSvgDom::from_bytes(data, FontMgr::default()).expect("Create svg dom failed");
 
         self.dom = Some(dom);
-    }
-
-    #[inline]
-    fn handle_geometry_changed(&mut self, rect: FRect) {
-        let (x1, y1, w1, h1) = (rect.x(), rect.y(), rect.width(), rect.height());
-        let (w2, h2) = (self.view_size.width() as f32, self.view_size.height() as f32);
-        self.origin = FPoint::new(x1 + (w1 - w2) / 2., y1 + (h1 - h2) / 2.);
     }
 }
